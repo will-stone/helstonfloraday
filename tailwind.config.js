d47b3911d7f60a182e3584d635b485598a326e84
Will Stone <654103+will-stone@users.noreply.github.com>
2024-05-11 21:31:09 +0100

    Start header, nav , and hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+    },
+    container: {
+      center: true,
+      padding: '1rem',
+    },
+    extend: {
+      colors: {
+        primary: '#2E7C71',
+        secondary: '#FFE9A8',
+      },
+      fontFamily: {
+        display: 'Oswald, Helvetica, Arial, sans-serif',
+        sans: '"Open Sans", ui-sans-serif, system-ui, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+      },
+    },
   },
   plugins: [],
 }
