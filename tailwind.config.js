3b13d4e4f53082626b25c014ce2c039fbe916f87
Will Stone <654103+will-stone@users.noreply.github.com>
2024-07-12 19:03:45 +0100

    Start gallery

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     screens: {
+      sm: '640px',
       md: '768px',
       lg: '1024px',
     },
