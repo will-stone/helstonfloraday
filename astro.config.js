eb7766e617459d5f2f907c66a33092a0d60fb8ca
Will Stone <654103+will-stone@users.noreply.github.com>
2026-05-04 13:39:46 +0100

    Upgrade all deps

--- astro.config.js
+++ astro.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
+import tailwindcss from '@tailwindcss/vite'
 
 import svelte from '@astrojs/svelte'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
+  integrations: [svelte()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })
