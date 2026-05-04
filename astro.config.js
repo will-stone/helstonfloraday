e75054eac5afd1195872736d97bdf2296df3e247
Will Stone <654103+will-stone@users.noreply.github.com>
2026-05-04 13:50:43 +0100

    Add sitemap

--- astro.config.js
+++ astro.config.js
@@ -3,9 +3,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 import svelte from '@astrojs/svelte'
 
+import sitemap from '@astrojs/sitemap'
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
+  site: 'https://helstonfloraday.org.uk',
+  integrations: [svelte(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
